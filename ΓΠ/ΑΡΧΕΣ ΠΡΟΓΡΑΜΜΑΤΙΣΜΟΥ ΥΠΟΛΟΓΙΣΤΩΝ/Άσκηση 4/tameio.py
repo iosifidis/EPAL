48eb4fd885d7153d98c5104c133c5724c4cc29cf
iosifidis <iefstathios@gmail.com>
2018-11-21 10:58:47 +0200

    Tameio

---
+++ ΓΠ/ΑΡΧΕΣ ΠΡΟΓΡΑΜΜΑΤΙΣΜΟΥ ΥΠΟΛΟΓΙΣΤΩΝ/Άσκηση 4/tameio.py
@@ -0,0 +1,19 @@
+﻿# δεδομένα 1
+timi=float(input('Τιμή προϊόντος/κιλού:'))
+posotita=float(input('Ποσότητα/βάρος προϊόντος:'))
+
+# επεξεργασία - υπολογισμοί
+synolo=timi * posotita
+fpa= synolo * 24/100
+posopliromis= synolo + fpa
+
+#εμφάνιση τελικού ποσού μαζί με ΦΠΑ
+print 'τελικό ποσό μαζί με ΦΠΑ:',round( posopliromis,2)
+
+# δεδομένα 2
+pliromi=float(input('Πληρωμή:'))
+
+# υπολογισμός - ρέστα
+resta = pliromi - posopliromis
+# εμφάνιση ρέστα
+print 'Ρέστα:',round(resta,2)
