34b31b73cf8473797470cc2f0015730224aa64e5
Efstathios Iosifidis <iefstathios@gmail.com>
2019-05-13 09:29:42 +0300

    typo

--- ΓΠ/ΣΧΕΔΙΑΣΜΟΣ ΚΑΙ ΑΝΑΠΤΥΞΗ ΔΙΑΔΙΚΤΥΑΚΩΝ ΕΦΑΡΜΟΓΩΝ/Εργαστηριακή άσκηση/form.php
+++ ΓΠ/ΣΧΕΔΙΑΣΜΟΣ ΚΑΙ ΑΝΑΠΤΥΞΗ ΔΙΑΔΙΚΤΥΑΚΩΝ ΕΦΑΡΜΟΓΩΝ/Εργαστηριακή άσκηση/form.php
@@ -6,7 +6,7 @@
 
   <div class="form-group">
     <label for="content">Content</label>
-    <textarea class="form-control" iname="content"></textarea>
+    <textarea class="form-control" name="content"></textarea>
   </div>
 <div class="form-group">
   <input type="submit" name="contact-submit" class="btn btn-primary" value="Αποστολή">
